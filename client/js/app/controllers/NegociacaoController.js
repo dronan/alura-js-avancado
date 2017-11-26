b3f6674c272f84b9801a3d663a6ac22384388f18
dronan <dronan@gmail.com>
2017-11-26 20:19:30 -0200

    outras formas de trabalhar data, com map e desmembramento de array com spread operator (...)

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -16,13 +16,24 @@ class NegociacaoController {
 		event.preventDefault();
 
 		//let data = new Date(this._inputData.value.split('-')); // O construtor de data aceita um array como valor
-		let data = new Date(this._inputData.value.replace(/-/g, ',')); // outra forma é dar um replace no hífen por virgula
+		//let data = new Date(this._inputData.value.replace(/-/g, ',')); // outra forma é dar um replace no hífen por virgula
+
+		// outra forma é fazer um mapa a partir da array, subtrair 1 do segundo numero para não dar problema em um 12-11-2016, para não ser entendido como dezembro, e o construtor da data receber a data formatada corretamente
+		let data = new Date(
+							...this._inputData.value
+											.split("-")
+											.map(function(item, indice){
+												return item - indice % 2
+											})
+			)
 
 		let negociacao = new Negociacao(
 				data,
 				this._inputQuantidade.value,
 				this._inputValor.value
 				);
+
+		console.log(negociacao);
 
 	}
 
