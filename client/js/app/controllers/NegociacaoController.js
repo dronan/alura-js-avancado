24ecd40ccaed75d9414b70cbc06a0a436be0c980
Diego Ronan <diegoronan@Mac-mini-de-Felipe.local>
2017-11-27 10:02:16 -0200

    isolando trabalho com data em outra classe para o controller

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -15,25 +15,15 @@ class NegociacaoController {
 	adiciona(event){
 		event.preventDefault();
 
-		//let data = new Date(this._inputData.value.split('-')); // O construtor de data aceita um array como valor
-		//let data = new Date(this._inputData.value.replace(/-/g, ',')); // outra forma é dar um replace no hífen por virgula
-
-		// outra forma é fazer um mapa a partir da array, subtrair 1 do segundo numero para não dar problema em um 12-11-2016, para não ser entendido como dezembro, e o construtor da data receber a data formatada corretamente
-		let data = new Date(
-							...this._inputData.value
-											.split("-")
-											.map((item, indice) => item - indice % 2) // arrow function, e ainda em uma linha, posso omitir o return e as chaves
-			)
+		let helper = new DateHelper();
 
 		let negociacao = new Negociacao(
-				data,
+				helper.textoParaData(this._inputData.value),
 				this._inputQuantidade.value,
 				this._inputValor.value
 				);
 
-		let diaMesAno = negociacao.data.getDate() +'/'+ (negociacao.data.getMonth() + 1) +'/'+negociacao.data.getFullYear(); 
-
-		console.log(diaMesAno);
+		let diaMesAno = helper.dataParaTexto(negociacao.data);
 
 	}
 
