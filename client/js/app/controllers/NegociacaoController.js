dac53ed33ce6d09dd39d48a11c1c2b619217941c
Diego Ronan <diegoronan@Mac-mini-de-Felipe.local>
2017-11-27 10:17:50 -0200

    criacao de metodos estaticos da classe helper; tratativa de erro caso a classe seja instanciada

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -15,15 +15,14 @@ class NegociacaoController {
 	adiciona(event){
 		event.preventDefault();
 
-		let helper = new DateHelper();
-
 		let negociacao = new Negociacao(
-				helper.textoParaData(this._inputData.value),
+				DateHelper.textoParaData(this._inputData.value),
 				this._inputQuantidade.value,
 				this._inputValor.value
 				);
 
-		let diaMesAno = helper.dataParaTexto(negociacao.data);
+		let diaMesAno = DateHelper.dataParaTexto(negociacao.data);
+
 
 	}
 
--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -1,10 +1,14 @@
 class DateHelper{
 
-	dataParaTexto(data){
+	constructor() {
+		throw new Error("Esta classe não pode ser instanciada");
+	}
+
+	static dataParaTexto(data){ // static  evita criar uma instancia (new Classe) e ocupar memoria
 		return data.getDate() +'/'+ (data.getMonth() + 1) +'/'+data.getFullYear(); 
 	}
-	
-	textoParaData(texto){
+
+	static textoParaData(texto){
 		return new Date(...texto.split("-").map((item, indice) => item - indice % 2)) // arrow function, e ainda em uma linha, posso omitir o return e as chaves
 	}
 
