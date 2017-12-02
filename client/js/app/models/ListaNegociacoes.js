83caf046dcfaaf249fdbbb15e5780c65e1bc00e6
dronan <dronan@gmail.com>
2017-12-02 15:19:14 -0200

    alterando o escopo da funcao anonima para contexto lexico, utilizando arrow function. Assim mantem o contexto no controller e nao na classe invocada; pode ser removido os itens de contexto da lista, bem como o reflection

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,25 +1,22 @@
 class ListaNegociacoes {
 
-	constructor(contexto, armadilha) {
+	constructor(armadilha) {
 		this._negociacoes = [];
 		this._armadilha = armadilha;
-		this._contexto = contexto;
 	}
 
 	adiciona(negociacao){
 		this._negociacoes.push(negociacao);
-		Reflect.apply(this._armadilha, this._contexto, [this]);
-		//this._armadilha(this);
+		this._armadilha(this);
 	}
 
 	get negociacoes() {
-		return [].concat(this._negociacoes); // o retorno e concatenacao de array, evita que um this._listaNegociacoes.negociacoes.push(this._criaNegociacao()); crie uma negociacao adicionando-a a lista, coisa que só o adiciona deve fazer, blindando a classe.
+		return [].concat(this._negociacoes); 
 	}
 
 	esvazia() {
 		this._negociacoes = [];
-		Reflect.apply(this._armadilha, this._contexto, [this]);
-		//this._armadilha(this);
+		this._armadilha(this);
 	}
 
 }
