13bedcf933e523c58ab5ed4e8afa3146d4d77f5f
dronan <dronan@gmail.com>
2017-12-16 18:59:39 -0200

    parse json da resposta do servidor, mapeando resultado e adicionando a novo objeto

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -70,9 +70,13 @@ class NegociacaoController {
 
 			if (xhr.readyState == 4) {
 				if (xhr.status == 200 ) {
-					console.log("Obtendo as informações do servidor");
+					JSON.parse(xhr.responseText)
+						.map (objeto =>  new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor ))
+						.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+					this._mensagem.texto = "Negociacoes importadas com sucesso!";
 				} else {
-					console.log("Não foi possivel obter as informações do servidor");
+					console.log(xhr.responseText);
+					this._mensagem.texto = "Não foi possivel obter as informações do servidor";
 				}
 			}
 		};
