cab4c48457b2a08143fa93ea3cfa6b2860d675b2
dronan <dronan@gmail.com>
2018-04-01 15:49:15 -0300

    tornando a conexao unica, independente das chamadas. Usando o Module Pattern, criando uma funcao anonima, auto executavel, chamando pelo nome da classe

--- client/js/app/services/ConnectionFactory.js
+++ client/js/app/services/ConnectionFactory.js
@@ -1,42 +1,49 @@
-var stores = ['negociacoes'];
-var version = 4;
-var dbname = 'aluraframe';
+var ConnectionFactory = (function () {
+	var stores = ['negociacoes'];
+	var version = 4;
+	var dbname = 'aluraframe';
+	var connection = null;
 
-class ConnectionFactory {
+	return class ConnectionFactory {
 
-	constructor() {
-		throw new Error ("Não é possivel instanciar esta classe");
+		constructor() {
+			throw new Error ("Não é possivel instanciar esta classe");
+		}
+
+		static getConnection() {
+			return new Promise((resolve, reject) => {
+				let openRequest = window.indexedDB.open(dbname, version);
+
+				openRequest.onupgradedneeded = e => {
+					ConnectionFactory._createStores(e.target.result);
+				};
+
+				openRequest.onsuccess = e => {
+
+					if(!connection) connection = e.target.result;
+
+					resolve(connection);
+
+				};
+
+				openRequest.onerror = e => {
+					console.log(e.target.error);
+					reject(e.target.error.name);
+				};
+
+			});
+		}
+
+		static _createStores(connection) {
+			stores.forEach(store => {
+
+				if (connection.objectStoreNames.contains(store)) 
+					connection.deleteObjectStore(store);
+
+				connection.createObjectStore(store, { autoIncrement: true});
+
+			});
+		}
+
 	}
-
-	static getConnection() {
-		return new Promise((resolve, reject) => {
-			let openRequest = window.indexedDB.open(dbname, version);
-
-			openRequest.onupgradedneeded = e => {
-				ConnectionFactory._createStores(e.target.result);
-			};
-
-			openRequest.onsuccess = e => {
-				resolve(e.target.result);
-			};
-
-			openRequest.onerror = e => {
-				console.log(e.target.error);
-				reject(e.target.error.name);
-			};
-
-		});
-	}
-
-	static _createStores(connection) {
-		stores.forEach(store => {
-
-			if (connection.objectStoreNames.contains(store)) 
-				connection.deleteObjectStore(store);
-
-			connection.createObjectStore(store, { autoIncrement: true});
-
-		});
-	}
-
-}
+})(); // funcao anonima auto executavel
